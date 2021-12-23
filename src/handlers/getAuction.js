023ee097a9d6efeb81a67d7b86c91da446c82310
Nilam Pathak <nilampathak72@gmail.com>
2021-12-23 11:58:24 +0530

    common middleware added

--- src/handlers/getAuction.js
+++ src/handlers/getAuction.js
@@ -1,8 +1,6 @@
 import AWS from 'aws-sdk';
 import middy from '@middy/core';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
+import commonMiddleware from '../lib/middleware';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -33,7 +31,4 @@ async function getAuction(event, context) {
     };
 }
 
-export const handler = middy(getAuction)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(getAuction);
